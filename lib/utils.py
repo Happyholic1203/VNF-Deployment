1cf2f9b61bfd2c8152b7f2729056e0efd364c2f8
Happyholic1203 <Happyholic1203@gmail.com>
2014-09-19 13:23:37 +0800

    Converted tabs to spaces

--- lib/utils.py
+++ lib/utils.py
@@ -4,139 +4,139 @@ import random
 import time
 
 class TestCase(object):
-	def __init__(self, name, flowSettings=None,
-		depth=3, fanout=2, minFlowAmount=0, maxFlowAmount=1,
-		minNumFlowsPerHost=2, maxNumFlowsPerHost=5):
-		if not self.checkFlowSettings(flowSettings):
-			raise Exception('Invalid flow settings: %r' % (flowSettings))
+    def __init__(self, name, flowSettings=None,
+        depth=3, fanout=2, minFlowAmount=0, maxFlowAmount=1,
+        minNumFlowsPerHost=2, maxNumFlowsPerHost=5):
+        if not self.checkFlowSettings(flowSettings):
+            raise Exception('Invalid flow settings: %r' % (flowSettings))
 
-		self.name = name
-		self.flowSettings = flowSettings
-		self.minNumFlowsPerHost = minNumFlowsPerHost
-		self.maxNumFlowsPerHost = maxNumFlowsPerHost
-		self.depth = depth
-		self.fanout = fanout
+        self.name = name
+        self.flowSettings = flowSettings
+        self.minNumFlowsPerHost = minNumFlowsPerHost
+        self.maxNumFlowsPerHost = maxNumFlowsPerHost
+        self.depth = depth
+        self.fanout = fanout
 
-		if self.flowSettings is None:
-			self.flowSettings = [
-				{
-					'minFlowAmount': minFlowAmount,
-					'maxFlowAmount': maxFlowAmount,
-					'ratio': 1
-				}
-			]
+        if self.flowSettings is None:
+            self.flowSettings = [
+                {
+                    'minFlowAmount': minFlowAmount,
+                    'maxFlowAmount': maxFlowAmount,
+                    'ratio': 1
+                }
+            ]
 
-		self.tree = self.buildTree(self.depth, self.fanout)
+        self.tree = self.buildTree(self.depth, self.fanout)
 
-	def reset(self):
-		self.tree.reset()
-		return self
+    def reset(self):
+        self.tree.reset()
+        return self
 
-	def buildTree(self, depth, fanout):
-		tree = Tree(depth=depth, fanout=fanout)
-		hosts = tree.getHosts()
-		for h in hosts:
-			for _ in range(self.getNumFlowsPerHost()):
-				h.addFlow(self.getFlowAmount())
-		return tree
+    def buildTree(self, depth, fanout):
+        tree = Tree(depth=depth, fanout=fanout)
+        hosts = tree.getHosts()
+        for h in hosts:
+            for _ in range(self.getNumFlowsPerHost()):
+                h.addFlow(self.getFlowAmount())
+        return tree
 
-	def getNumFlowsPerHost(self):
-		return random.randint(
-			self.minNumFlowsPerHost,
-			self.maxNumFlowsPerHost)
+    def getNumFlowsPerHost(self):
+        return random.randint(
+            self.minNumFlowsPerHost,
+            self.maxNumFlowsPerHost)
 
-	def getFlowAmount(self):
-		uniform = random.uniform(0, 1.0)
-		accumulatedProb = 0
-		for flowSetting in self.flowSettings:
-			accumulatedProb += flowSetting['ratio']
-			if accumulatedProb >= uniform:
-				return random.uniform(flowSetting['minFlowAmount'], flowSetting['maxFlowAmount'])
-		raise Exception('The ratio in flowSettings do not add up to 1.0')
+    def getFlowAmount(self):
+        uniform = random.uniform(0, 1.0)
+        accumulatedProb = 0
+        for flowSetting in self.flowSettings:
+            accumulatedProb += flowSetting['ratio']
+            if accumulatedProb >= uniform:
+                return random.uniform(flowSetting['minFlowAmount'], flowSetting['maxFlowAmount'])
+        raise Exception('The ratio in flowSettings do not add up to 1.0')
 
-	# TODO
-	def run(self, solvers, drawPng=None, pngOutputDir=None, printVmPlacement=True, printSummary=True):
-		for s in solvers:
-			assert isinstance(s, Solver)
-		if not pngOutputDir:
-			pngOutputDir = '.'
+    # TODO
+    def run(self, solvers, drawPng=None, pngOutputDir=None, printVmPlacement=True, printSummary=True):
+        for s in solvers:
+            assert isinstance(s, Solver)
+        if not pngOutputDir:
+            pngOutputDir = '.'
 
-		allFlows = self.tree.getFlows()
-		numFlows = len(allFlows)
-		numHosts = len(self.tree.getHosts())
-		flowAmounts = [f.getCapacity() for f in allFlows]
-		flowsPerHostList = [len(flows) for flows in [h.getFlows() for h in self.tree.getHosts()]]
-		totalFlowAmount = self.tree.getTotalFlowAmount()
-		print '** TestCase: %s' % self.name
-		print '** Number of Hosts: %d' % numHosts
-		print '** Number of Flows per Host: (min, max, avg) = (%d, %d, %.2f)' % (
-			min(flowsPerHostList),
-			max(flowsPerHostList),
-			(float(numFlows) / float(numHosts)))
-		print '** Total Number of Flows: %d' % numFlows
-		print '** Flow Amount: (min, max, avg) = (%.2f, %.2f, %.2f)' % (
-			min(flowAmounts),
-			max(flowAmounts),
-			(totalFlowAmount / float(numFlows)))
-		print '** Total Flow Amount: %.2f' % totalFlowAmount
+        allFlows = self.tree.getFlows()
+        numFlows = len(allFlows)
+        numHosts = len(self.tree.getHosts())
+        flowAmounts = [f.getCapacity() for f in allFlows]
+        flowsPerHostList = [len(flows) for flows in [h.getFlows() for h in self.tree.getHosts()]]
+        totalFlowAmount = self.tree.getTotalFlowAmount()
+        print '** TestCase: %s' % self.name
+        print '** Number of Hosts: %d' % numHosts
+        print '** Number of Flows per Host: (min, max, avg) = (%d, %d, %.2f)' % (
+            min(flowsPerHostList),
+            max(flowsPerHostList),
+            (float(numFlows) / float(numHosts)))
+        print '** Total Number of Flows: %d' % numFlows
+        print '** Flow Amount: (min, max, avg) = (%.2f, %.2f, %.2f)' % (
+            min(flowAmounts),
+            max(flowAmounts),
+            (totalFlowAmount / float(numFlows)))
+        print '** Total Flow Amount: %.2f' % totalFlowAmount
 
-		result = {
-			'name': self.name,
-			'numHosts': numHosts,
-			'flowsPerHost': {
-				'min': min(flowsPerHostList),
-				'max': max(flowsPerHostList),
-				'avg': (float(numFlows) / float(numHosts))
-			},
-			'totalNumFlows': numFlows,
-			'flowAmount': {
-				'min': min(flowAmounts),
-				'max': max(flowAmounts),
-				'avg': (totalFlowAmount / float(numFlows))
-			},
-			'totalFlowAmount': totalFlowAmount
-		}
+        result = {
+            'name': self.name,
+            'numHosts': numHosts,
+            'flowsPerHost': {
+                'min': min(flowsPerHostList),
+                'max': max(flowsPerHostList),
+                'avg': (float(numFlows) / float(numHosts))
+            },
+            'totalNumFlows': numFlows,
+            'flowAmount': {
+                'min': min(flowAmounts),
+                'max': max(flowAmounts),
+                'avg': (totalFlowAmount / float(numFlows))
+            },
+            'totalFlowAmount': totalFlowAmount
+        }
 
-		records = []
-		for solver in solvers:
-			startTime = time.time()
-			solver.solve(self.tree)
-			print '** Runtime [%s]: %.2f seconds' % \
-				(solver.getName(), time.time() - startTime)
-			if drawPng:
-				self.tree.draw(
-					'%s/%s(%s).png' % (pngOutputDir, self.name, solver.getName()),
-					showFlowCapacity=True,
-					showFlowVm=True)
-			records.append({
-				'solver': solver.getName(),
-				'numVms': solver.getNumVms(),
-				'cost': solver.getTotalCost()
-			})
-			# print solver.getSolution(
-			# 	showVmPlacement=printVmPlacement,
-			# 	showSummary=printSummary)
-			self.reset()
-			solver.reset()
+        records = []
+        for solver in solvers:
+            startTime = time.time()
+            solver.solve(self.tree)
+            print '** Runtime [%s]: %.2f seconds' % \
+                (solver.getName(), time.time() - startTime)
+            if drawPng:
+                self.tree.draw(
+                    '%s/%s(%s).png' % (pngOutputDir, self.name, solver.getName()),
+                    showFlowCapacity=True,
+                    showFlowVm=True)
+            records.append({
+                'solver': solver.getName(),
+                'numVms': solver.getNumVms(),
+                'cost': solver.getTotalCost()
+            })
+            # print solver.getSolution(
+            #   showVmPlacement=printVmPlacement,
+            #   showSummary=printSummary)
+            self.reset()
+            solver.reset()
 
-		result['records'] = records
-		self.printRecords(records)
-		return result
+        result['records'] = records
+        self.printRecords(records)
+        return result
 
-	@staticmethod
-	def printRecords(records):
-		fmt = '{:>20}{:>10}{:>10}'
-		print fmt.format('solver', 'numVms', 'cost')
-		print '---------------------------------------------'
-		for rec in records:
-			print fmt.format(
-				rec['solver'],
-				rec['numVms'],
-				'%.2f' % rec['cost'])
-		print ''
+    @staticmethod
+    def printRecords(records):
+        fmt = '{:>20}{:>10}{:>10}'
+        print fmt.format('solver', 'numVms', 'cost')
+        print '---------------------------------------------'
+        for rec in records:
+            print fmt.format(
+                rec['solver'],
+                rec['numVms'],
+                '%.2f' % rec['cost'])
+        print ''
 
-	# TODO
-	def checkFlowSettings(self, flowSettings):
-		if not flowSettings:
-			return True
-		return True
+    # TODO
+    def checkFlowSettings(self, flowSettings):
+        if not flowSettings:
+            return True
+        return True
--- main.py
+++ main.py
@@ -4,113 +4,113 @@ from lib.ds import *
 from lib.utils import *
 
 if __name__ == '__main__':
-	proposedSolver02 = AlphaSolver(alpha=0.2)
-	proposedSolver04 = AlphaSolver(alpha=0.4)
-	proposedSolver06 = AlphaSolver(alpha=0.6)
-	proposedSolver08 = AlphaSolver(alpha=0.8)
-	leastCostSolver = AlphaSolver(alpha=0.0001)
-	leastVnfSolver = LeastVnfSolver()
+    proposedSolver02 = AlphaSolver(alpha=0.2)
+    proposedSolver04 = AlphaSolver(alpha=0.4)
+    proposedSolver06 = AlphaSolver(alpha=0.6)
+    proposedSolver08 = AlphaSolver(alpha=0.8)
+    leastCostSolver = AlphaSolver(alpha=0.0001)
+    leastVnfSolver = LeastVnfSolver()
 
-	defaultNumFlowsPerHost = [
-		(1, 5)
-		# (5, 10),
-		# (10, 50),
-		# (100, 500),
-		# (1000, 5000),
-		# (10000, 50000)
-	]
+    defaultNumFlowsPerHost = [
+        (1, 5)
+        # (5, 10),
+        # (10, 50),
+        # (100, 500),
+        # (1000, 5000),
+        # (10000, 50000)
+    ]
 
-	defaultFanout = 2
+    defaultFanout = 2
 
-	params = [
-		{
-			'name': 'Small Flows: [0:0.1]',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-				'minFlowAmount': 0,
-				'maxFlowAmount': 0.1,
-				'ratio': 1
-			}]
-		}, {
-			'name': 'Large Flows: [0.9:1]',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-				'minFlowAmount': 0.7,
-				'maxFlowAmount': 1,
-				'ratio': 1
-			}]
-		}, {
-			'name': 'Medium Flows: [0.45:0.55]',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-				'minFlowAmount': 0.45,
-				'maxFlowAmount': 0.55,
-				'ratio': 1
-			}]
-		}, {
-			'name': 'Small and Large Flows [0:0.1] : [0.9:1] = 1 : 1',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-					'minFlowAmount': 0,
-					'maxFlowAmount': 0.1,
-					'ratio': 0.5
-				}, {
-					'minFlowAmount': 0.9,
-					'maxFlowAmount': 1,
-					'ratio': 0.5
-				}
-			]
-		}, {
-			'name': 'Small and Medium Flows [0:0.1] : [0.45:0.55] = 1 : 1',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-					'minFlowAmount': 0,
-					'maxFlowAmount': 0.1,
-					'ratio': 0.5
-				}, {
-					'minFlowAmount': 0.45,
-					'maxFlowAmount': 0.55,
-					'ratio': 0.5
-				}
-			]
-		}, {
-			'name': 'Random Flows [0:1]',
-			'numFlowsPerHost': defaultNumFlowsPerHost,
-			'fanout': defaultFanout,
-			'flowSettings': [{
-					'minFlowAmount': 0,
-					'maxFlowAmount': 1,
-					'ratio': 1
-				}
-			]
-		}
-	]
+    params = [
+        {
+            'name': 'Small Flows: [0:0.1]',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                'minFlowAmount': 0,
+                'maxFlowAmount': 0.1,
+                'ratio': 1
+            }]
+        }, {
+            'name': 'Large Flows: [0.9:1]',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                'minFlowAmount': 0.7,
+                'maxFlowAmount': 1,
+                'ratio': 1
+            }]
+        }, {
+            'name': 'Medium Flows: [0.45:0.55]',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                'minFlowAmount': 0.45,
+                'maxFlowAmount': 0.55,
+                'ratio': 1
+            }]
+        }, {
+            'name': 'Small and Large Flows [0:0.1] : [0.9:1] = 1 : 1',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                    'minFlowAmount': 0,
+                    'maxFlowAmount': 0.1,
+                    'ratio': 0.5
+                }, {
+                    'minFlowAmount': 0.9,
+                    'maxFlowAmount': 1,
+                    'ratio': 0.5
+                }
+            ]
+        }, {
+            'name': 'Small and Medium Flows [0:0.1] : [0.45:0.55] = 1 : 1',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                    'minFlowAmount': 0,
+                    'maxFlowAmount': 0.1,
+                    'ratio': 0.5
+                }, {
+                    'minFlowAmount': 0.45,
+                    'maxFlowAmount': 0.55,
+                    'ratio': 0.5
+                }
+            ]
+        }, {
+            'name': 'Random Flows [0:1]',
+            'numFlowsPerHost': defaultNumFlowsPerHost,
+            'fanout': defaultFanout,
+            'flowSettings': [{
+                    'minFlowAmount': 0,
+                    'maxFlowAmount': 1,
+                    'ratio': 1
+                }
+            ]
+        }
+    ]
 
-	results = []
+    results = []
 
-	for param in params:
-		for minNumFlowsPerHost, maxNumFlowsPerHost in param['numFlowsPerHost']:
-			results.append(TestCase(
-					param['name'],
-					minNumFlowsPerHost=minNumFlowsPerHost,
-					maxNumFlowsPerHost=maxNumFlowsPerHost,
-					flowSettings=param['flowSettings'],
-					fanout=param['fanout']
-				).run([
-						leastCostSolver,
-						proposedSolver02,
-						proposedSolver04,
-						proposedSolver06,
-						proposedSolver08,
-						leastVnfSolver
-					],
-					printVmPlacement=False,
-					pngOutputDir='png',
-					drawPng=False))
+    for param in params:
+        for minNumFlowsPerHost, maxNumFlowsPerHost in param['numFlowsPerHost']:
+            results.append(TestCase(
+                    param['name'],
+                    minNumFlowsPerHost=minNumFlowsPerHost,
+                    maxNumFlowsPerHost=maxNumFlowsPerHost,
+                    flowSettings=param['flowSettings'],
+                    fanout=param['fanout']
+                ).run([
+                        leastCostSolver,
+                        proposedSolver02,
+                        proposedSolver04,
+                        proposedSolver06,
+                        proposedSolver08,
+                        leastVnfSolver
+                    ],
+                    printVmPlacement=False,
+                    pngOutputDir='png',
+                    drawPng=False))
 
-	print results
+    print results
