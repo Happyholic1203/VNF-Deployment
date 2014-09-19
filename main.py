4f4f982b4a7721047581c66795ad1dafd9569b07
Happyholic1203 <Happyholic1203@gmail.com>
2014-09-19 13:05:38 +0800

    Refactored main.py to enable fanout specification

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ from lib.ds import *
 from lib.utils import *
 
 if __name__ == '__main__':
-	proposedSolver = AlphaSolver(alpha=0.8)
+	proposedSolver02 = AlphaSolver(alpha=0.2)
+	proposedSolver04 = AlphaSolver(alpha=0.4)
+	proposedSolver06 = AlphaSolver(alpha=0.6)
+	proposedSolver08 = AlphaSolver(alpha=0.8)
 	leastCostSolver = AlphaSolver(alpha=0.0001)
 	leastVnfSolver = LeastVnfSolver()
 
-	numFlowsPerHostSettings = [
-		(1, 5),
-		(5, 10),
-		(10, 50),
-		(100, 500),
-		(1000, 5000),
-		(10000, 50000)
+	defaultNumFlowsPerHost = [
+		(1, 5)
+		# (5, 10),
+		# (10, 50),
+		# (100, 500),
+		# (1000, 5000),
+		# (10000, 50000)
 	]
+
+	defaultFanout = 2
 
 	params = [
 		{
 			'name': 'Small Flows: [0:0.1]',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 				'minFlowAmount': 0,
 				'maxFlowAmount': 0.1,
@@ -28,7 +34,8 @@ if __name__ == '__main__':
 			}]
 		}, {
 			'name': 'Large Flows: [0.9:1]',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 				'minFlowAmount': 0.7,
 				'maxFlowAmount': 1,
@@ -36,7 +43,8 @@ if __name__ == '__main__':
 			}]
 		}, {
 			'name': 'Medium Flows: [0.45:0.55]',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 				'minFlowAmount': 0.45,
 				'maxFlowAmount': 0.55,
@@ -44,7 +52,8 @@ if __name__ == '__main__':
 			}]
 		}, {
 			'name': 'Small and Large Flows [0:0.1] : [0.9:1] = 1 : 1',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 					'minFlowAmount': 0,
 					'maxFlowAmount': 0.1,
@@ -57,7 +66,8 @@ if __name__ == '__main__':
 			]
 		}, {
 			'name': 'Small and Medium Flows [0:0.1] : [0.45:0.55] = 1 : 1',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 					'minFlowAmount': 0,
 					'maxFlowAmount': 0.1,
@@ -70,7 +80,8 @@ if __name__ == '__main__':
 			]
 		}, {
 			'name': 'Random Flows [0:1]',
-			'numFlowsPerHostSettings': numFlowsPerHostSettings,
+			'numFlowsPerHost': defaultNumFlowsPerHost,
+			'fanout': defaultFanout,
 			'flowSettings': [{
 					'minFlowAmount': 0,
 					'maxFlowAmount': 1,
@@ -83,13 +94,21 @@ if __name__ == '__main__':
 	results = []
 
 	for param in params:
-		for minNumFlowsPerHost, maxNumFlowsPerHost in param['numFlowsPerHostSettings']:
+		for minNumFlowsPerHost, maxNumFlowsPerHost in param['numFlowsPerHost']:
 			results.append(TestCase(
-				param['name'],
+					param['name'],
 					minNumFlowsPerHost=minNumFlowsPerHost,
 					maxNumFlowsPerHost=maxNumFlowsPerHost,
-					flowSettings=param['flowSettings']
-				).run([proposedSolver, leastCostSolver, leastVnfSolver],
+					flowSettings=param['flowSettings'],
+					fanout=param['fanout']
+				).run([
+						leastCostSolver,
+						proposedSolver02,
+						proposedSolver04,
+						proposedSolver06,
+						proposedSolver08,
+						leastVnfSolver
+					],
 					printVmPlacement=False,
 					pngOutputDir='png',
 					drawPng=False))
